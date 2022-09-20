bc3d6e8f954fa24648d5906028dfe5c7b8135f40
yk9978 <ammarmahmood@Ammars-MacBook-Air.local>
2022-09-20 16:01:59 +0200

    Starting

---
+++ Longest_Substring_Unique.py
@@ -0,0 +1,19 @@
+from itertools import count
+
+
+var = str(input("Enter a word with repeating characters: "))
+
+def split_characters(m):
+    for i in m:
+        return i
+    
+def lengthofsubstring(p):
+    m=[]
+    print(len(p))
+
+
+    
+m = map(split_characters,var)
+p = list(m)
+lengthofsubstring(p)
+
