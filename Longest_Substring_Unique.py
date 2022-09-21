dbfc500812f4488e4a3f84d2fa3cb2672c2797eb
Ammar Mahmood <ammarmahmood94@gmail.com>
2022-09-21 09:13:12 +0200

    unresolved

--- Longest_Substring_Unique.py
+++ Longest_Substring_Unique.py
@@ -4,8 +4,7 @@ from itertools import count
 var = str(input("Enter a word with repeating characters: "))
 
 def split_characters(m):
-    for i in m:
-        return i
+    return m
     
 def lengthofsubstring(p):
     
@@ -18,28 +17,17 @@ def lengthofsubstring(p):
         if i not in m:
             m.append(i)
             countkeep.append(count-1)
-    return m, countkeep
+    return countkeep
         
-def new_func(m, countkeep):
-    
-    new = []
-    new.append(0)
-    k=[]
+def new_func(countkeep):
 
     for i in range(len(countkeep)):
         if i < (len(countkeep) - 1):
-            k.append(countkeep[i+1] - countkeep[i])
-        
-
-    # t = ""
-    
-    # for i in new:
-    #     t+=p[i]
-    
-    # print(f"The longest substring is: {t} and its length is: {len(new)}")
+            pass
 
 m = map(split_characters, var)
 p = list(m)
-list, counter = lengthofsubstring(p)
-new_func(list, counter)
+counter = lengthofsubstring(p)
+print(counter)
+new_func(counter)
 
